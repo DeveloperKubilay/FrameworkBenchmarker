16786e4fdd9a561502e8dca4fc09898caa80cdae
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-12-23 10:58:11 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ autocannon -c 500 -d 30 -p 10 http://localhost:3000
 
 
 
-timeout 30s ab -n 100000 -c 500 http://localhost:8080/
+timeout 10s ab -n 100000 -c 500 http://localhost:8080/ > result.txt && cat result.txt 
 
 sudo apt install apache2-utils   # Debian/Ubuntu
 sudo yum install httpd-tools     # CentOS/RHEL
